d0ae8bddbfd97687fd4d16ab1345fcf979f0b267
baloran <contact@baloran.fr>
2018-07-22 13:55:24 +0200

    chore(init): embed gist thanks to remarkable plugin

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const parse = md => {
+  console.log(md.src.charCodeAt(md.pos))
+  if (md.src.charCodeAt(md.pos) !== 123) {
+    return false
+  }
+
+  const match = /{gist\s+([\S]+?)}/.exec(md.src.slice(md.pos))
+  if (!match) return false
+
+  md.pos += match[0].length
+
+  const token = {
+    type: 'embedGist',
+    level: md.level,
+    content: {
+      gist: match[1],
+      match: match,
+    },
+  }
+
+  md.push(token)
+  return true
+}
+
+const render = (tokens, idx) => {
+  const token = tokens[idx]
+
+  return `<script src="https://gist.github.com/${token.content.gist}" />`
+}
+
+const embedGist = ctx => {
+  ctx.inline.ruler.push('embedGist', parse)
+  ctx.renderer.rules['embedGist'] = render
+}
+
+export default embedGist
---
+++ src/index.spec.js
@@ -0,0 +1,18 @@
+import Remarkable from 'remarkable'
+import embedGist from './index'
+
+let md
+
+describe('Embed gist into documentation', () => {
+  beforeAll(() => {
+    md = new Remarkable()
+    md.use(embedGist)
+  })
+  it('can correctly embed gist into remarkable', () => {
+    expect(
+      md.render(
+        'Salut je suis un gist {gist baloran/d1564cf2c60471b3844be86507c29f45}',
+      ),
+    ).toBeNull()
+  })
+})
